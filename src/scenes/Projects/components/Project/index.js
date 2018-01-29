e8c0b1f098bc1b7c4942387bf42eea8a7d242d3d
Shawn Lauzon <shawn.lauzon@gmail.com>
2018-01-28 23:12:42 -0500

    Integrate Material-UI

--- src/scenes/Projects/components/Project/index.js
+++ src/scenes/Projects/components/Project/index.js
@@ -1,10 +1,33 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { withStyles } from 'material-ui/styles'
+import Card, { CardContent } from 'material-ui/Card'
+import Typography from 'material-ui/Typography'
 
-const Project = (props) => <div>Project: {props.name}</div>
-
-Project.propTypes = {
-  name: PropTypes.string.isRequired
+const styles = {
+  card: {
+    maxWidth: 345
+  }
 }
 
-export default Project
+function Project (props) {
+  const { classes } = props
+  return (
+    <div>
+      <Card className={classes.card}>
+        <CardContent>
+          <Typography type='headline' component='h2'>
+            {props.name}
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
+
+Project.propTypes = {
+  name: PropTypes.string.isRequired,
+  classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(Project)
