e3f284b17a686d049d5232369f647e65400cf457
Shawn Lauzon <shawn.lauzon@gmail.com>
2018-01-29 15:20:05 -0500

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import React, { Component } from 'react'
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 import Projects from './scenes/Projects'
 import './App.css'
 
-class App extends Component {
-  render () {
-    return (
-      <Projects />
-    )
-  }
-}
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+const ProjectsScene = () => (
+  <div>
+    <Projects />
+  </div>
+)
+
+const App = () => (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/projects'>ProjectsScene</Link></li>
+      </ul>
+
+      <hr />
+
+      <Route exact path='/' component={Home} />
+      <Route path='/projects' component={ProjectsScene} />
+    </div>
+  </Router>
+)
 
 export default App
