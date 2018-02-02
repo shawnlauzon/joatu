a773a4e307ed5a80c3f1b1ae778d1b920043399d
Shawn Lauzon <shawn.lauzon@gmail.com>
2018-02-01 20:56:55 -0500

    Integrate with Redux chrome extensions

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import { devToolsEnhancer } from 'redux-devtools-extension'
 import projects from './data'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
-const store = createStore(projects)
+const store = createStore(projects, devToolsEnhancer())
 
 ReactDOM.render(
   <Provider store={store}>
