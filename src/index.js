7a656915b332d3ab94be253b66a8985631ff84a6
Shawn Lauzon <shawn.lauzon@gmail.com>
2018-02-04 00:23:22 -0500

    Remove index.css

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import './index.css'
 
 import projects from './data'
 import { firebase } from './firebaseBackend/core'
