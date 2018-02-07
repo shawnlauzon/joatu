1e8cba7b9b5b6cffe011d8fb969347c4edd397f6
Shawn Lauzon <shawn.lauzon@gmail.com>
2018-02-06 19:49:32 -0500

    Forgot to modify root actions.js

--- src/data/actions.js
+++ src/data/actions.js
@@ -1,7 +1,7 @@
 import * as communityActions from './communities/actions'
 import * as projectActions from './projects/actions'
 import * as userActions from './users/actions'
-import * as authActions from './user/actions'
+import * as authActions from './authenticated/actions'
 
 export { communityActions, projectActions, userActions, authActions }
 
