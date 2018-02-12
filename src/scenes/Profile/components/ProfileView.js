d677463a19925d28fa6075527e9a13047ff97fba
Shawn Lauzon <shawn.lauzon@gmail.com>
2018-02-12 13:02:06 -0500

    Include links to PORs

--- src/scenes/Profile/components/ProfileView.js
+++ src/scenes/Profile/components/ProfileView.js
@@ -1,50 +1,61 @@
 import { isEmpty } from 'ramda'
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 import Typography from 'material-ui/Typography'
+import Avatar from 'material-ui/Avatar'
 import List, { ListItem, ListItemText } from 'material-ui/List'
 
 const Profile = props => (
   <div>
-    <Typography variant="display3">
-      {props.user && props.user.displayName}
-    </Typography>
-    <Typography variant="display2">Projects</Typography>
-    {isEmpty(props.projects) ? (
-      <Typography variant="body1">None</Typography>
-    ) : (
-      <List>
-        {Object.entries(props.projects).map(([id, value]) => (
-          <ListItem key={id}>
-            <ListItemText primary={value.name} />
-          </ListItem>
-        ))}
-      </List>
-    )}
-    <Typography variant="display2">Offers</Typography>
-    {isEmpty(props.offers) ? (
-      <Typography variant="body1">None</Typography>
-    ) : (
-      <List>
-        {Object.entries(props.offers).map(([id, value]) => (
-          <ListItem key={id}>
-            <ListItemText primary={value.name} />
-          </ListItem>
-        ))}
-      </List>
-    )}
-    <Typography variant="display2">Requests</Typography>
-    {isEmpty(props.requests) ? (
-      <Typography variant="body1">None</Typography>
-    ) : (
-      <List>
-        {Object.entries(props.requests).map(([id, value]) => (
-          <ListItem key={id}>
-            <ListItemText primary={value.name} />
-          </ListItem>
-        ))}
-      </List>
+    {props.user && (
+      <div>
+        <Avatar src={props.user.imgUrl} />
+        <Typography variant="display3">{props.user.displayName}</Typography>
+        <Typography variant="display2">Projects</Typography>
+        {isEmpty(props.projects) ? (
+          <Typography variant="body1">None</Typography>
+        ) : (
+          <List>
+            {Object.entries(props.projects).map(([id, value]) => (
+              <ListItem key={id}>
+                <ListItemText
+                  primary={<Link to={`/projects/${id}`}>{value.name}</Link>}
+                />
+              </ListItem>
+            ))}
+          </List>
+        )}
+        <Typography variant="display2">Offers</Typography>
+        {isEmpty(props.offers) ? (
+          <Typography variant="body1">None</Typography>
+        ) : (
+          <List>
+            {Object.entries(props.offers).map(([id, value]) => (
+              <ListItem key={id}>
+                <ListItemText
+                  primary={<Link to={`/offers/${id}`}>{value.name}</Link>}
+                />
+              </ListItem>
+            ))}
+          </List>
+        )}
+        <Typography variant="display2">Requests</Typography>
+        {isEmpty(props.requests) ? (
+          <Typography variant="body1">None</Typography>
+        ) : (
+          <List>
+            {Object.entries(props.requests).map(([id, value]) => (
+              <ListItem key={id}>
+                <ListItemText
+                  primary={<Link to={`/requests/${id}`}>{value.name}</Link>}
+                />
+              </ListItem>
+            ))}
+          </List>
+        )}
+      </div>
     )}
   </div>
 )
