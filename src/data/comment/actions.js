c79d647adaee6acb4e9d01e8867b7c4f6ffdaa05
Shawn Lauzon <shawn.lauzon@gmail.com>
2018-02-21 01:50:52 -0500

    Remove last bad db link

--- src/data/comment/actions.js
+++ src/data/comment/actions.js
@@ -8,10 +8,10 @@ export const CREATE_COMMENT_STARTED = 'CREATE_COMMENT_STARTED'
 export const CREATE_COMMENT_SUCCEEDED = 'CREATE_COMMENT_SUCCEEDED'
 export const CREATE_COMMENT_FAILED = 'CREATE_COMMENT_FAILED'
 
-export const ADD_NEW_COMMENT_TO_USER_STARTED = 'ADD_NEW_COMMENT_TO_USER_STARTED'
-export const ADD_NEW_COMMENT_TO_USER_SUCCEEDED =
-  'ADD_NEW_COMMENT_TO_USER_SUCCEEDED'
-export const ADD_NEW_COMMENT_TO_USER_FAILED = 'ADD_NEW_COMMENT_TO_USER_FAILED'
+// export const ADD_NEW_COMMENT_TO_USER_STARTED = 'ADD_NEW_COMMENT_TO_USER_STARTED'
+// export const ADD_NEW_COMMENT_TO_USER_SUCCEEDED =
+//   'ADD_NEW_COMMENT_TO_USER_SUCCEEDED'
+// export const ADD_NEW_COMMENT_TO_USER_FAILED = 'ADD_NEW_COMMENT_TO_USER_FAILED'
 
 export const UPDATE_COMMENT_STARTED = 'UPDATE_COMMENT_STARTED'
 export const UPDATE_COMMENT_SUCCEEDED = 'UPDATE_COMMENT_SUCCEEDED'
@@ -21,11 +21,11 @@ export const DELETE_COMMENT_STARTED = 'DELETE_COMMENT_STARTED'
 export const DELETE_COMMENT_SUCCEEDED = 'DELETE_COMMENT_SUCCEEDED'
 export const DELETE_COMMENT_FAILED = 'DELETE_COMMENT_FAILED'
 
-export const REMOVE_COMMENT_FROM_USER_STARTED =
-  'REMOVE_COMMENT_FROM_USER_STARTED'
-export const REMOVE_COMMENT_FROM_USER_SUCCEEDED =
-  'REMOVE_COMMENT_FROM_USER_SUCCEEDED'
-export const REMOVE_COMMENT_FROM_USER_FAILED = 'REMOVE_COMMENT_FROM_USER_FAILED'
+// export const REMOVE_COMMENT_FROM_USER_STARTED =
+//   'REMOVE_COMMENT_FROM_USER_STARTED'
+// export const REMOVE_COMMENT_FROM_USER_SUCCEEDED =
+//   'REMOVE_COMMENT_FROM_USER_SUCCEEDED'
+// export const REMOVE_COMMENT_FROM_USER_FAILED = 'REMOVE_COMMENT_FROM_USER_FAILED'
 
 const doFetchComments = () => ({
   [CALL_API]: {
@@ -55,24 +55,24 @@ const doCreateComment = body => ({
   }
 })
 
-const doAddNewCommentToUser = (commentId, ownerId) => ({
-  [CALL_API]: {
-    types: [
-      ADD_NEW_COMMENT_TO_USER_STARTED,
-      ADD_NEW_COMMENT_TO_USER_SUCCEEDED,
-      ADD_NEW_COMMENT_TO_USER_FAILED
-    ],
-    collection: 'users',
-    action: 'addRef',
-    category: 'comments',
-    fromId: ownerId,
-    toId: commentId
-  }
-})
+// const doAddNewCommentToUser = (commentId, ownerId) => ({
+//   [CALL_API]: {
+//     types: [
+//       ADD_NEW_COMMENT_TO_USER_STARTED,
+//       ADD_NEW_COMMENT_TO_USER_SUCCEEDED,
+//       ADD_NEW_COMMENT_TO_USER_FAILED
+//     ],
+//     collection: 'users',
+//     action: 'addRef',
+//     category: 'comments',
+//     fromId: ownerId,
+//     toId: commentId
+//   }
+// })
 
 export const create = body => async (dispatch, getState) => {
   const result = await dispatch(doCreateComment(body))
-  dispatch(doAddNewCommentToUser(result.payload.id, result.payload.data.to))
+  // dispatch(doAddNewCommentToUser(result.payload.id, result.payload.data.to))
 }
 
 const doUpdateComment = (id, body) => ({
@@ -106,25 +106,25 @@ const doDeleteComment = id => ({
   }
 })
 
-const doRemoveCommentFromUser = (commentId, ownerId) => ({
-  [CALL_API]: {
-    types: [
-      REMOVE_COMMENT_FROM_USER_STARTED,
-      REMOVE_COMMENT_FROM_USER_SUCCEEDED,
-      REMOVE_COMMENT_FROM_USER_FAILED
-    ],
-    collection: 'users',
-    action: 'removeRef',
-    category: 'comments',
-    fromId: ownerId,
-    toId: commentId
-  }
-})
+// const doRemoveCommentFromUser = (commentId, ownerId) => ({
+//   [CALL_API]: {
+//     types: [
+//       REMOVE_COMMENT_FROM_USER_STARTED,
+//       REMOVE_COMMENT_FROM_USER_SUCCEEDED,
+//       REMOVE_COMMENT_FROM_USER_FAILED
+//     ],
+//     collection: 'users',
+//     action: 'removeRef',
+//     category: 'comments',
+//     fromId: ownerId,
+//     toId: commentId
+//   }
+// })
 
 export const remove = id => (dispatch, getState) => {
-  const comment = getState().comments[id]
+  // const comment = getState().comments[id]
   return Promise.all([
-    dispatch(doDeleteComment(id)),
-    dispatch(doRemoveCommentFromUser(id, comment.owner))
+    dispatch(doDeleteComment(id))
+    // dispatch(doRemoveCommentFromUser(id, comment.owner))
   ])
 }
