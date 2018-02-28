1637669e082d7f282c760eb061ac8b6a93facf52
Shawn Lauzon <shawn.lauzon@gmail.com>
2018-02-28 14:52:00 -0500

    Fix warning <p> cannot be under <div>

--- src/scenes/Hub/scenes/CreateProject/index.js
+++ src/scenes/Hub/scenes/CreateProject/index.js
@@ -50,7 +50,7 @@ const CreateProjectForm = ({
           />
         </Grid>
         <Grid item className={classes.text}>
-          <Typography variant="body1">
+          <Typography component="span" variant="body1">
             We will meet at{' '}
             <TextField
               name="place"
