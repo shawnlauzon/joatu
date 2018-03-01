4644bab6b561b802100ad8986cbca443b983045b
Shawn Lauzon <shawn.lauzon@gmail.com>
2018-03-01 17:02:21 -0500

    Cleanup Chat

--- src/data/chat/actions.js
+++ src/data/chat/actions.js
@@ -1,7 +1,6 @@
 import * as R from 'ramda'
 import { CALL_API } from '../actions'
 import { boolMap } from '../utils'
-// import { authenticatedUser } from '../user/selectors'
 
 export const FETCH_CHATS_STARTED = 'FETCH_CHATS_STARTED'
 export const FETCH_CHATS_SUCCEEDED = 'FETCH_CHATS_SUCCEEDED'
@@ -15,10 +14,6 @@ export const CREATE_CHAT_STARTED = 'CREATE_CHAT_STARTED'
 export const CREATE_CHAT_SUCCEEDED = 'CREATE_CHAT_SUCCEEDED'
 export const CREATE_CHAT_FAILED = 'CREATE_CHAT_FAILED'
 
-// export const ADD_NEW_CHAT_TO_USER_STARTED = 'ADD_NEW_CHAT_TO_USER_STARTED'
-// export const ADD_NEW_CHAT_TO_USER_SUCCEEDED = 'ADD_NEW_CHAT_TO_USER_SUCCEEDED'
-// export const ADD_NEW_CHAT_TO_USER_FAILED = 'ADD_NEW_CHAT_TO_USER_FAILED'
-
 export const UPDATE_CHAT_STARTED = 'UPDATE_CHAT_STARTED'
 export const UPDATE_CHAT_SUCCEEDED = 'UPDATE_CHAT_SUCCEEDED'
 export const UPDATE_CHAT_FAILED = 'UPDATE_CHAT_FAILED'
@@ -26,10 +21,6 @@ export const UPDATE_CHAT_FAILED = 'UPDATE_CHAT_FAILED'
 export const DELETE_CHAT_STARTED = 'DELETE_CHAT_STARTED'
 export const DELETE_CHAT_SUCCEEDED = 'DELETE_CHAT_SUCCEEDED'
 export const DELETE_CHAT_FAILED = 'DELETE_CHAT_FAILED'
-
-// export const REMOVE_CHAT_FROM_USER_STARTED = 'REMOVE_CHAT_FROM_USER_STARTED'
-// export const REMOVE_CHAT_FROM_USER_SUCCEEDED = 'REMOVE_CHAT_FROM_USER_SUCCEEDED'
-// export const REMOVE_CHAT_FROM_USER_FAILED = 'REMOVE_CHAT_FROM_USER_FAILED'
 
 export const FETCH_MESSAGES_STARTED = 'FETCH_MESSAGES_STARTED'
 export const FETCH_MESSAGES_SUCCEEDED = 'FETCH_MESSAGES_SUCCEEDED'
@@ -97,11 +88,6 @@ export const create = body => (dispatch, getState) => {
   const evolveBody = R.over(R.lensProp('participants'), boolMap)
 
   dispatch(doCreateChat(evolveBody(body)))
-  // const participant1 = result.payload.data.participants[0]
-  // const participant2 = result.payload.data.participants[1]
-
-  // dispatch(doAddNewChatToUser(result.payload.id, participant1, participant2))
-  // dispatch(doAddNewChatToUser(result.payload.id, participant2, participant1))
 }
 
 const doUpdateChat = (id, body) => ({
@@ -127,26 +113,6 @@ const doDeleteChat = id => ({
   }
 })
 
-// const doRemoveChatFromUser = (chatId, ownerId) => ({
-//   [CALL_API]: {
-//     types: [
-//       REMOVE_CHAT_FROM_USER_STARTED,
-//       REMOVE_CHAT_FROM_USER_SUCCEEDED,
-//       REMOVE_CHAT_FROM_USER_FAILED
-//     ],
-//     collection: 'users',
-//     action: 'removeRef',
-//     category: 'chats',
-//     fromId: ownerId,
-//     toId: chatId
-//   }
-// })
-
 export const remove = id => (dispatch, getState) => {
-  // const chat = getState().chats[id]
   dispatch(doDeleteChat(id))
-
-  // chat.participants.forEach(participant => {
-  //   dispatch(doRemoveChatFromUser(id, participant))
-  // })
 }
--- src/data/chat/selectors.js
+++ src/data/chat/selectors.js
@@ -4,14 +4,6 @@ import orm from '../orm'
 
 import { inflateUser } from '../utils'
 
-export const offersInHub = createSelector(
-  orm,
-  state => state.db,
-  state => state.selectedHubId,
-  (session, hubId) =>
-    session.Offer.filter(offer => offer.hub === hubId).toModelArray()
-)
-
 const hasUser = id => R.find(R.propEq('id', id))
 
 export const chatWithUser = userId =>
@@ -41,30 +33,3 @@ export const chatWithUser = userId =>
       return matchingChats ? matchingChats[0] : undefined
     }
   )
-
-// export const newMessages = chatId =>
-//   createSelector(
-//     orm,
-//     state => state.db,
-//     session => {
-//       const matchingChats = session.Chat.all()
-//         .toModelArray()
-//         .filter(chat => {
-//           return R.both(hasUser(userId), hasUser(authenticatedUserId))(
-//             chat.participants.toRefArray()
-//           )
-//         })
-//         .map(chat => {
-//           const participants = chat.participants.toRefArray()
-//           return Object.assign({}, chat.ref, {
-//             participants: participants.map(inflateUser)
-//           })
-//         })
-
-//       if (matchingChats.length > 1) {
-//         console.error('Warning: expected 0 or 1 chats', matchingChats)
-//       }
-
-//       return matchingChats ? matchingChats[0] : undefined
-//     }
-//   )
